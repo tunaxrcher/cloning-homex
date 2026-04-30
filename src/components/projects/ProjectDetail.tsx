24ea31d062ef72e00fbdce74ef2bde56c13bbfc9
tunaxrcher <mystic.outriderxpaine@gmail.com>
2026-04-30 17:54:52 +0700

    .

--- src/components/projects/ProjectDetail.tsx
+++ src/components/projects/ProjectDetail.tsx
@@ -942,14 +942,14 @@ const ProjectDetail = ({
       </div>
 
       <div className="w-full">
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-2 bg-default-100 dark:bg-zinc-800/50 p-1.5 rounded-2xl w-full">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-2 bg-default-100 dark:bg-zinc-800/50 p-1.5 rounded-2xl w-full">
           {[
             {
               id: "dashboard",
               label: "สรุป",
               icon: <LayoutDashboard size={18} />,
             },
-            { id: "tasks", label: "งาน", icon: <Building2 size={18} /> },
+            // { id: "tasks", label: "งาน", icon: <Building2 size={18} /> },
             {
               id: "taskv2",
               label: (
